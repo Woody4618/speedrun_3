a10c5c57b89da56b3d3d32a36de8a0daadae7155
Jonas Hahn <herzock@gmail.com>
2024-06-30 19:16:50 +0100

    Solana Games Preset + program

---
+++ lastforever/app/pages/index.tsx
@@ -0,0 +1,32 @@
+import { Box, Flex, Heading, Spacer, VStack, Text } from "@chakra-ui/react"
+import { useWallet } from "@solana/wallet-adapter-react"
+import WalletMultiButton from "@/components/WalletMultiButton"
+import DisplayGameState from "@/components/DisplayGameState"
+import InitPlayerButton from "@/components/InitPlayerButton"
+import SessionKeyButton from "@/components/SessionKeyButton"
+import ChopTreeButton from "@/components/ChopTreeButton"
+import RequestAirdrop from "@/components/RequestAirdrop"
+import DisplayNfts from "@/components/DisplayNfts"
+
+export default function Home() {
+  const { publicKey } = useWallet()
+
+  return (
+    <Box>
+      <Flex px={4} py={4}>
+        <Spacer />
+        <WalletMultiButton />
+      </Flex>
+      <VStack>
+        <Heading>Lastforever</Heading>
+        {!publicKey && <Text>Connect to devnet wallet!</Text>}
+        <DisplayGameState />
+        <InitPlayerButton />
+        <SessionKeyButton />
+        <ChopTreeButton />
+        <RequestAirdrop />
+        <DisplayNfts />
+      </VStack>
+    </Box>
+  )
+}
---
+++ lastforever/program/tests/lumberjack.ts
@@ -0,0 +1,86 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Lastforever } from "../target/types/lastforever";
+
+describe("lastforever", () => {
+
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
+  const program = anchor.workspace.Lastforever as Program<Lastforever>;
+  const payer = provider.wallet as anchor.Wallet
+  const gameDataSeed = "gameData";
+
+  it("Init player and chop tree!", async () => {
+
+    console.log("Local address", payer.publicKey.toBase58());
+
+    const balance = await anchor.getProvider().connection.getBalance(payer.publicKey);
+    
+    if (balance < 1e8) {
+      const res = await anchor.getProvider().connection.requestAirdrop(payer.publicKey, 1e9);
+      await anchor.getProvider().connection.confirmTransaction(res, "confirmed");  
+    }
+
+    const [playerPDA] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("player"),
+        payer.publicKey.toBuffer(),        
+      ],
+      program.programId
+    );
+
+    console.log("Player PDA", playerPDA.toBase58());
+
+    const [gameDataPDA] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        Buffer.from(gameDataSeed)       
+      ],
+      program.programId
+    );
+
+    try {
+      let tx = await program.methods.initPlayer(gameDataSeed)
+      .accounts(
+        {
+          player: playerPDA,
+          gameData: gameDataPDA,
+          signer: payer.publicKey,        
+          systemProgram: anchor.web3.SystemProgram.programId,
+        }    
+      )
+      .rpc({skipPreflight: true});
+      console.log("Init transaction", tx);
+      
+      await anchor.getProvider().connection.confirmTransaction(tx, "confirmed");
+      console.log("Confirmed", tx);
+
+    } catch (e) {
+      console.log("Player already exists: ", e);
+    }
+    
+    for (let i = 0; i < 11; i++) {
+      console.log(`Chop instruction ${i}`);
+
+      let tx = await program.methods
+      .chopTree(gameDataSeed, 0)
+      .accounts(
+        {
+          sessionToken: null,
+          player: playerPDA,
+          gameData: gameDataPDA,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          signer: payer.publicKey
+        }    
+      )
+      .rpc({skipPreflight: true});
+      console.log("Chop instruction", tx);
+      await anchor.getProvider().connection.confirmTransaction(tx, "confirmed");
+    }
+
+    const accountInfo = await anchor.getProvider().connection.getAccountInfo(
+      playerPDA, "confirmed"
+    );
+    const decoded = program.coder.accounts.decode("PlayerData", accountInfo.data);
+    console.log("Player account info", JSON.stringify(decoded));
+  });
+});
