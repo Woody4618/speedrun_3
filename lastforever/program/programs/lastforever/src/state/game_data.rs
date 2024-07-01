27b93df38022efa0734a29ee0981723a9987e13b
Jonas Hahn <herzock@gmail.com>
2024-07-01 18:02:54 +0100

    Update game_data.rs

--- lastforever/program/programs/lastforever/src/state/game_data.rs
+++ lastforever/program/programs/lastforever/src/state/game_data.rs
@@ -2,28 +2,31 @@ use anchor_lang::prelude::*;
 
 use crate::constants::MAX_WOOD_PER_TREE;
 
+use super::player_data::PlayerData;
+
 #[account]
 pub struct GameData {
-    pub total_wood_collected: u64,
+  pub total_wood_collected: u64,
+  pub players: Vec<PlayerData>,
 }
 
 impl GameData {
-    pub fn on_tree_chopped(&mut self, amount_chopped: u64) -> Result<()> {
-        match self.total_wood_collected.checked_add(amount_chopped) {
-            Some(v) => {
-                if self.total_wood_collected >= MAX_WOOD_PER_TREE {
-                    self.total_wood_collected = 0;
-                    msg!("Tree successfully chopped. New Tree coming up.");
-                } else {
-                    self.total_wood_collected = v;
-                    msg!("Total wood chopped: {}", v);
-                }
-            }
-            None => {
-                msg!("The ever tree is completly chopped!");
-            }
-        };
-
-        Ok(())
+  pub fn on_tree_chopped(&mut self, amount_chopped: u64) -> Result<()> {
+    match self.total_wood_collected.checked_add(amount_chopped) {
+      Some(v) => {
+        if self.total_wood_collected >= MAX_WOOD_PER_TREE {
+          self.total_wood_collected = 0;
+          msg!("Tree successfully chopped. New Tree coming up.");
+        } else {
+          self.total_wood_collected = v;
+          msg!("Total wood chopped: {}", v);
+        }
+      }
+      None => {
+        msg!("The ever tree is completly chopped!");
+      }
     }
+
+    Ok(())
+  }
 }
